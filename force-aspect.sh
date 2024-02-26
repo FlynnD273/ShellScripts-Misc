b98fa020ed78c06b221f10cebf7f34ff4d55ed45
Flynn Duniho <flynn@harveyosity.org>
2024-02-26 23:07:19 +0000

    make force-aspect also work on jpg images

--- force-aspect.sh
+++ force-aspect.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 mkdir bordered
-find . -type f -exec ffmpeg -i "{}" -vf "scale=4088:3064:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" \;
+find *.png -exec ffmpeg -i "{}" -vf "scale=4088:3064:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" \;
+find *.jpg -exec ffmpeg -i "{}" -vf "scale=4088:3064:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" \;
