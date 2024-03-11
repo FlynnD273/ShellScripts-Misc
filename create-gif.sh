c0e643c940db8baa34ce67896d7f038f7a79e7c5
Flynn Duniho <flynn@harveyosity.org>
2024-03-11 18:18:40 -0400

    add Markdown to PDF script, fix create-gif script

--- create-gif.sh
+++ create-gif.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-ffmpeg -i $1 -filter_complex "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse" ${@:2}
+ffmpeg -i $1 -filter_complex "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse" $2
