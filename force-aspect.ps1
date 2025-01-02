38affd870b17f4268e548cf36cd22abb1e3ffbff
Flynn Duniho <flynn@harveyosity.org>
2025-01-02 01:21:54 -0800

    Make force-aspect uset he max dimensions for Instagram

--- force-aspect.ps1
+++ force-aspect.ps1
@@ -1,4 +1,10 @@
 mkdir bordered
-foreach ($file in (Get-ChildItem "*.png" -File)){& ffmpeg -i $($file.Name) -vf "scale=4088:3064:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/$($file.name)"}
-foreach ($file in (Get-ChildItem "*.jpg" -File)){& ffmpeg -i $($file.Name) -vf "scale=4088:3064:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/$($file.name)"}
-foreach ($file in (Get-ChildItem "*.jpeg" -File)){& ffmpeg -i $($file.Name) -vf "scale=4088:3064:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/$($file.name)"}
+foreach ($file in (Get-ChildItem "*.png" -File))
+{& ffmpeg -i $($file.Name) -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=1080:810:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/$($file.name)"
+}
+foreach ($file in (Get-ChildItem "*.jpg" -File))
+{& ffmpeg -i $($file.Name) -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=1080:810:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/$($file.name)"
+}
+foreach ($file in (Get-ChildItem "*.jpeg" -File))
+{& ffmpeg -i $($file.Name) -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=1080:810:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/$($file.name)"
+}
--- force-aspect.sh
+++ force-aspect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir bordered
-find *.png -exec ffmpeg -i "{}" -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" -y \;
-find *.jpg -exec ffmpeg -i "{}" -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" -y \;
-find *.jpeg -exec ffmpeg -i "{}" -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=4088:3064:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" -y \;
+find *.png -exec ffmpeg -i "{}" -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=1080:810:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" -y \;
+find *.jpg -exec ffmpeg -i "{}" -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=1080:810:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" -y \;
+find *.jpeg -exec ffmpeg -i "{}" -vf "scale=1080:810:force_original_aspect_ratio=decrease,pad=1080:810:(ow-iw)/2:(oh-ih)/2,setsar=1" "bordered/{}" -y \;
