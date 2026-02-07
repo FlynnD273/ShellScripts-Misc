cb331486e9634847ec70fceda838f5f314769782
Flynn Duniho <flynn@harveyosity.org>
2026-02-06 23:05:54 -0800

    Update fusing scripts to use DNG files instead

--- fuse.sh
+++ fuse.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 output="$2"
 if [ ! "$output" ]; then
-	name="$(basename "$1")"
-	output="${name%_[0-9].jpg}_hdr"
+	output="${1%_[0-9].dng}_hdr"
 fi
-file="${1%_[0-9].jpg}"
+file="${1%_[0-9].dng}"
 
-for f in "$file"_[0-9]*.jpg; do
-	t=$(mktemp --suffix .jpg)
+for f in "$file"_[0-9]*.dng; do
+	t=$(mktemp --suffix .tiff)
 	ffmpeg -i "$f" "$t" -y
 	exiftool -overwrite_Original -TagsFromFile "$f" -All:All "$t"
-	mv "$t" "$f" -f
+	mv "$t" "${f%.dng}.tiff" -f
 done
 
-align_image_stack "$file"_[0-9]*.jpg -o "$output"
+align_image_stack "$file"_[0-9]*.tiff -o "$output"
 ffmpeg -i "$output.hdr" -compression_algo 32946 "$output.tiff" -y && rm "$output.hdr"
--- fuseall.sh
+++ fuseall.sh
@@ -6,14 +6,16 @@ if [ ! -d ~/Downloads/a ]; then
 	mkdir ~/Downloads/a
 fi
 
-for file in "$folder/"IMG_*_*_[0-9].jpg; do
+for file in "$folder/"IMG_*_*_[0-9].dng; do
 	if [ ! -f "$file" ]; then
 		continue
 	fi
 	d=$(date -R -r "$file")
-	output="${file%_[0-9].jpg}_hdr"
+	output="${file%_[0-9].dng}_hdr"
 	fuse "$file" "$output"
 	gio trash "${file%_[0-9].jpg}"_[0-9].jpg
+	gio trash "${file%_[0-9].jpg}"_[0-9].dng
 	touch -d "$d" "$output.tiff"
 	cp "$output.tiff" ~/Downloads/a
+	exit
 done
