279bdc6f70ac623c9855fe926b240552ac0cf5a6
Flynn Duniho <flynn@harveyosity.org>
2026-02-15 17:30:23 -0800

    Remove exit

--- setwallpaper.sh
+++ setwallpaper.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 file=$(ls ~/Pictures/Wallpapers/*.* | shuf -n 1)
+echo "Chose file $file"
 
 # hyprctl hyprpaper preload "$file"
 # hyprctl hyprpaper wallpaper ,contain:"$file"
 # hyprctl hyprpaper unload all
 gsettings set org.gnome.desktop.background picture-uri-dark "$file"
+
+# For Plasma
+
+plasma-apply-wallpaperimage "$file"
+echo Switched wallpaper.
