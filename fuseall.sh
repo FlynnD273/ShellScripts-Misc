447741d63a711ccb2b9324940900cedd245ed21f
Flynn Duniho <flynn@harveyosity.org>
2026-04-04 20:14:32 -0700

    Make a jpg version too since apparently dng messes up exposure sometimes

--- fuseall.sh
+++ fuseall.sh
@@ -11,7 +11,7 @@ for file in "$folder/"IMG_*_*_[0-9].dng; do
 		continue
 	fi
 	d=$(date -R -r "$file")
-	output="${file%_[0-9].dng}_hdr"
+	output="${file%_[0-9].dng}_dng_hdr"
 	if [[ -f  "$output.tiff" ]]; then
 		rm "$output.tiff"
 	fi
@@ -23,8 +23,24 @@ for file in "$folder/"IMG_*_*_[0-9].dng; do
 		fuse "$file" "$output"
 		filesize=$(stat -c%s "$output.tiff")
 	done
+
+	output_jpg="${file%_[0-9].dng}_jpg_hdr"
+	if [[ -f  "$output_jpg.tiff" ]]; then
+		rm "$output_jpg.tiff"
+	fi
+	fuse "${file%.dng}.jpg" "$output_jpg"
+	filesize=$(stat -c%s "$output_jpg.tiff")
+	while [[ "$filesize" -eq 0 ]]; do
+		echo "Conversion for ${file%_[0-9].jpg} failed. Retrying..."
+		rm "$output_jpg.tiff"
+		fuse "$file" "$output_jpg"
+		filesize=$(stat -c%s "$output_jpg.tiff")
+	done
+
 	gio trash "${file%_[0-9].dng}"_[0-9].jpg
 	gio trash "${file%_[0-9].dng}"_[0-9].dng
 	touch -d "$d" "$output.tiff"
+	touch -d "$d" "$output_jpg.tiff"
 	mv "$output.tiff" ~/Downloads/a -f
+	mv "$output_jpg.tiff" ~/Downloads/a -f
 done
